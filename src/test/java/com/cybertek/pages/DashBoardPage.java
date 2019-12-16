73d207636d20b43e3e2bc6d8fbd74ebb81fd8c14
Fatma <fatmas199021@gmail.com>
2019-12-16 22:54:07 +0300

    day14 pom_tests

---
+++ src/test/java/com/cybertek/pages/DashBoardPage.java
@@ -0,0 +1,13 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashBoardPage extends BasePage {
+
+    //no ned to explicitly write constructors, because it will use its parents constructor.
+
+    public DashBoardPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+}
