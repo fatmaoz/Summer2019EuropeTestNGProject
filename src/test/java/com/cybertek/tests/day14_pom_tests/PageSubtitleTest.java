c19c83ddb5695daede736b6d754692f5987867fe
Fatma <fatmas199021@gmail.com>
2019-12-16 23:03:00 +0300

    day14 pom_tests

--- src/test/java/com/cybertek/tests/day14_pom_tests/PageSubtitleTest.java
+++ src/test/java/com/cybertek/tests/day14_pom_tests/PageSubtitleTest.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PageSubtitleTest extends TestBase {
-
+//you need sthing
     @Test
     public void verifySubTitleTest() {
 
